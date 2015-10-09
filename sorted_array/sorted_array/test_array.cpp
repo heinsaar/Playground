08911be6e54da33bcddfc577cf9f8c3a459b724c
Leo Heinsaar <leo.heinsaar@hotmail.com>
2015-10-09 14:49:49 +0400

    Refactored and split into separate files for further extensibility.

---
+++ sorted_array/sorted_array/test_array.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <chrono>
+
+void test_array()
+{
+    // Generate data
+    const int SIZE = 1500;
+    int data[SIZE];
+
+    for (int c = 0; c < SIZE; ++c)
+    {
+        data[c] = std::rand() % 256;
+    }
+
+    // With sort, the primary loop runs faster; why?
+    std::sort(data, data + SIZE);
+
+    auto start = std::chrono::steady_clock::now();
+
+    long sum = 0;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        // Primary loop
+        for (int k = 0; k < SIZE; ++k)
+        {
+            if (data[k] >= 128)
+            {
+                sum += data[k];
+            }
+        }
+    }
+
+    auto end = std::chrono::steady_clock::now();
+
+    std::chrono::duration<double> duration = end - start;
+    std::cout << duration.count() << std::endl;
+    std::cout << "sum = " << sum << std::endl;
+    std::cin.get();
+}
